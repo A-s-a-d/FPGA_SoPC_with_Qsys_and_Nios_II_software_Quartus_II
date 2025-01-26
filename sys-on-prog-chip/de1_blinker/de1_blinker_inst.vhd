bc9c25afc6578d6b26503ccdac6350a1afdeb07f
A-s-a-d <81926998+A-s-a-d@users.noreply.github.com>
2025-01-26 21:34:35 +0100

    i guessed fixed the write permission issue

--- sys-on-prog-chip/de1_blinker/de1_blinker_inst.vhd
+++ sys-on-prog-chip/de1_blinker/de1_blinker_inst.vhd
@@ -2,11 +2,11 @@
 		port (
 			clk_clk                                : in  std_logic                    := 'X';             -- clk
 			led_external_connection_export         : out std_logic_vector(7 downto 0);                    -- export
-			switcher_external_connection_export    : in  std_logic_vector(3 downto 0) := (others => 'X'); -- export
 			seven_seg_1_external_connection_export : out std_logic_vector(7 downto 0);                    -- export
 			seven_seg_2_external_connection_export : out std_logic_vector(7 downto 0);                    -- export
 			seven_seg_3_external_connection_export : out std_logic_vector(7 downto 0);                    -- export
-			seven_seg_4_external_connection_export : out std_logic_vector(7 downto 0)                     -- export
+			seven_seg_4_external_connection_export : out std_logic_vector(7 downto 0);                    -- export
+			switcher_external_connection_export    : in  std_logic_vector(3 downto 0) := (others => 'X')  -- export
 		);
 	end component de1_blinker;
 
@@ -14,10 +14,10 @@
 		port map (
 			clk_clk                                => CONNECTED_TO_clk_clk,                                --                             clk.clk
 			led_external_connection_export         => CONNECTED_TO_led_external_connection_export,         --         led_external_connection.export
-			switcher_external_connection_export    => CONNECTED_TO_switcher_external_connection_export,    --    switcher_external_connection.export
 			seven_seg_1_external_connection_export => CONNECTED_TO_seven_seg_1_external_connection_export, -- seven_seg_1_external_connection.export
 			seven_seg_2_external_connection_export => CONNECTED_TO_seven_seg_2_external_connection_export, -- seven_seg_2_external_connection.export
 			seven_seg_3_external_connection_export => CONNECTED_TO_seven_seg_3_external_connection_export, -- seven_seg_3_external_connection.export
-			seven_seg_4_external_connection_export => CONNECTED_TO_seven_seg_4_external_connection_export  -- seven_seg_4_external_connection.export
+			seven_seg_4_external_connection_export => CONNECTED_TO_seven_seg_4_external_connection_export, -- seven_seg_4_external_connection.export
+			switcher_external_connection_export    => CONNECTED_TO_switcher_external_connection_export     --    switcher_external_connection.export
 		);
 
